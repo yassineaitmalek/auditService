36eb630f6f291c7c57942a19aecf2fe9c7fb3e37
yassineaitmalek <yassineaitmalek7@gmail>
2023-09-29 11:31:33 +0100

    audit Service

--- src/main/java/com/test/auditService/controller/TestController.java
+++ src/main/java/com/test/auditService/controller/TestController.java
@@ -1,10 +1,13 @@
 package com.test.auditService.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.test.auditService.model.Audit4jDB;
 import com.test.auditService.service.TestService;
 
 import lombok.RequiredArgsConstructor;
@@ -15,6 +18,12 @@ import lombok.RequiredArgsConstructor;
 class TestController {
 
     private final TestService testService;
+
+    @GetMapping("/audit")
+    public List<Audit4jDB> audit() {
+        return testService.getAudit();
+
+    }
 
     @GetMapping("/hello")
     public String hello() {
--- src/main/java/com/test/auditService/service/TestService.java
+++ src/main/java/com/test/auditService/service/TestService.java
@@ -1,8 +1,13 @@
 package com.test.auditService.service;
 
+import java.util.List;
+
 import org.audit4j.core.annotation.Audit;
 import org.audit4j.core.annotation.IgnoreAudit;
 import org.springframework.stereotype.Service;
+
+import com.test.auditService.model.Audit4jDB;
+import com.test.auditService.repository.Audit4jDBRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -10,6 +15,12 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class TestService {
+
+  private final Audit4jDBRepository audit4jDBRepository;
+
+  public List<Audit4jDB> getAudit() {
+    return audit4jDBRepository.findAll();
+  }
 
   public String hello() {
     return "hello";
